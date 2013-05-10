07b7475ea6dfb221013f469265512acaeebac2d9
Bojidar Penchev <bojidar_penchev@hotmail.com>
2013-05-10 17:57:04 +0300

    add new const NUMBER_OF_DIGITS  in NumberGenerator

--- ConsolePrinter.cs
+++ ConsolePrinter.cs
@@ -9,6 +9,7 @@ namespace BullsAndCowsGame
                                                "Please try to guess my secret 4-digit number." +
                                                "Use 'top' to view the top scoreboard, 'restart' " +
                                                "to start a new game and 'help' to cheat and 'exit' to quit the game.";
+       
         private const string WRONG_COMMAND_MESSAGE = "Incorrect guess or command!";
         
         public static void PrintWelcomeMessage()
--- NumberGenerator.cs
+++ NumberGenerator.cs
@@ -8,20 +8,20 @@ namespace BullsAndCowsGame
     public static class NumberGenerator
     {
         private const int NUMBER_LENGHT = 4;
-
+        private const int NUMBER_OF_DIGITS = 9;
         public static string GenerateNumber()
         {
-            
-            StringBuilder num = new StringBuilder(4);
+
+            StringBuilder num = new StringBuilder(NUMBER_LENGHT);
             Random randomNumberGenerator = new Random(DateTime.Now.Millisecond);
             List<int> usedDigits = new List<int>();
 
             for (int i = 0; i < NUMBER_LENGHT; i++)
             {
-                int randomDigit = randomNumberGenerator.Next(9);
+                int randomDigit = randomNumberGenerator.Next(NUMBER_OF_DIGITS);
                 while (usedDigits.Contains(randomDigit))
                 {
-                    randomDigit = randomNumberGenerator.Next(9);
+                    randomDigit = randomNumberGenerator.Next(NUMBER_OF_DIGITS);
                 }
                 usedDigits.Add(randomDigit);
                 num.Append(randomDigit);
