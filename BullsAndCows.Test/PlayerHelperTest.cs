976c57ce3218924668f09fae8023dad3160365de
Bunny303 <bunny@gmail.com>
2013-05-11 11:33:12 +0300

    Make test for methods in class PlayerHelper;
    Test fot ClearHelp() don't work correctly!

--- BullsAndCows.Test/PlayerHelperTest.cs
+++ BullsAndCows.Test/PlayerHelperTest.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using BullsAndCowsGame;
+using System.IO;
+using System.Text;
 
 namespace BullsAndCows.Test
 {
@@ -8,22 +11,61 @@ namespace BullsAndCows.Test
     [TestClass()]
     public class PlayerHelperTest
     {
-        
+        private static StringBuilder helpNumber = new StringBuilder("XXXX");
+         
         [TestMethod()]
-        public void ClearHelpTest()
+        [ExpectedException(typeof(ArgumentException))]
+        public void PrintHelpTest1()
         {
-           
+            PlayerHelper.PrintHelp(1, string.Empty);
         }
 
         [TestMethod()]
-        public void PrintHelpTest()
+        [ExpectedException(typeof(ArgumentException))]
+        public void PrintHelpTest2()
         {
-            //int cheats = 0; // TODO: Initialize to an appropriate value
-            //string generatedNumber = string.Empty; // TODO: Initialize to an appropriate value
-            //int expected = 0; // TODO: Initialize to an appropriate value
-            //int actual;
-            //actual = PlayerHelper.PrintHelp(cheats, generatedNumber);
-            //Assert.AreEqual(expected, actual);
+            PlayerHelper.PrintHelp(1, null);
+        }
+        
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PrintHelpTest3()
+        {
+            PlayerHelper.PrintHelp(-3, "5678");
+        }
+
+        [TestMethod()]
+        public void PrintHelpTest4()
+        {
+            int cheats = 2;
+            int expected = cheats+1;
+            int actual = PlayerHelper.PrintHelp(cheats, "5789");
+            Assert.AreEqual(expected, actual, "Cheats are calculated wrong!");
+        }
+        
+        [TestMethod()]
+        public void PrintHelpTest5()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                
+                PlayerHelper.PrintHelp(5, "5678");
+
+                string expected = string.Format("You are not allowed to ask for more help!{0}", Environment.NewLine);
+                Assert.AreEqual<string>(expected, sw.ToString(), "Wrong message printed!");
+
+                sw.Flush();
+            }
+        }
+
+        [TestMethod()]
+        public void ClearHelpTest()
+        {
+            StringBuilder expected = new StringBuilder("XXXX");
+            PlayerHelper.ClearHelp();
+
+            Assert.AreEqual(expected, helpNumber, "Help number is not cleared correctly!");
         }
     }
 }
