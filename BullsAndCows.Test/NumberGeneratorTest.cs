47ef7b957eee32921d11d754924c6516b29d3692
Bojidar Penchev <bojidar_penchev@hotmail.com>
2013-05-15 15:22:48 +0300

    Add some test in for randomGenerator class

--- BullsAndCows.Test/NumberGeneratorTest.cs
+++ BullsAndCows.Test/NumberGeneratorTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BullsAndCowsGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace BullsAndCows.Test
@@ -6,14 +7,46 @@ namespace BullsAndCows.Test
     [TestClass()]
     public class NumberGeneratorTest
     {
-        
         [TestMethod()]
-        public void GenerateNumberTest()
+        public void GenerateNumberTestIfNumbersAreDifferent()
         {
-            //string expected = string.Empty; // TODO: Initialize to an appropriate value
-            //string actual;
-            //actual = NumberGenerator.GenerateNumber();
-            //Assert.AreEqual(expected, actual);
+            for (int j = 0; j < 50000; j++)
+            {
+                string actual = NumberGenerator.GenerateNumber();
+                for (int i = 0; i < actual.Length - 1; i++)
+                {
+                    Assert.AreNotEqual(actual[i], actual[i + 1]);
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void GenerateNumberTestIsValidDigitInRange()
+        {
+            for (int j = 0; j < 50000; j++)
+            {
+                string actual = NumberGenerator.GenerateNumber();
+                for (int i = 0; i < actual.Length - 1; i++)
+                {
+                    if (int.Parse(actual[i].ToString()) > 9 && int.Parse(actual[i].ToString()) < 0)
+                    {
+                        Assert.Fail("Invalid Number");
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void GenerateNumberTestCheckNumberLength()
+        {
+            for (int j = 0; j < 50000; j++)
+            {
+                string actual = NumberGenerator.GenerateNumber();
+                for (int i = 0; i < actual.Length - 1; i++)
+                {
+                    Assert.AreEqual(4, actual.Length);
+                }
+            }
         }
     }
 }
